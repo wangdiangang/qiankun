8587c26fb6c7bc4bae7ef46bbea837faf1f94997
wudaogang <wangdiangang9788@sina.com>
2021-01-06 11:16:09 +0800

    init

---
+++ sub-vue-next/src/views/About.vue
@@ -0,0 +1,11 @@
+<!--
+ * @Author: 五道杠
+-->
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <ul>
+      <li v-for="i in 200" :key="i">哈哈哈哈{{i}}</li>
+    </ul>
+  </div>
+</template>
